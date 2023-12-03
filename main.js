3e59d24b1bf44c4d6284dcd38f3584b47746c235
Bassam-Z <bassamz.ali@hotmail.com>
2023-12-03 16:20:21 +0100

    Map Data Type     -Methods

--- main.js
+++ main.js
@@ -2908,3 +2908,48 @@ Map Data Type
 
 // console.log(myNewMap);
 // console.log(myNewObject);
+
+// ***************************************************
+
+/*
+    Map Data Type
+    -Methods
+        - set
+        - get
+        - delete
+        - clear
+        - has
+
+    Properties
+        - size
+*/ 
+
+// let myMap = new Map([
+//     [10, "Number"],
+//     ["Name", "String"],
+//     [false, "Boolean"]
+// ]);
+
+// // myMap.set(10, "Number");
+// // myMap.set("Name", "String");
+
+// console.log(myMap);
+
+// console.log(myMap.get(10));
+// console.log(myMap.get("Name"));
+// console.log(myMap.get(false));
+
+// console.log("##################");
+
+// console.log(myMap.has(false));
+
+// console.log("##################");
+
+// console.log(myMap.size);
+
+// console.log(myMap.delete("Name"));
+// console.log(myMap.size);
+
+// myMap.clear();
+// console.log(myMap.size);
+
